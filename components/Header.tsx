04576add3efe6f44d25825225d65bfc2b14b4c1b
Dra1noo <D3marpayne@gmail.com>
2025-10-03 13:01:17 -0400

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  // simple header, nothing fancy
+  return (
+    <header>
+      <h1>Welcome to My React App</h1>
+      <p>This is an introductory React app using components and props.</p>
+    </header>
+  );
+};
+
+export default Header;
